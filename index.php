e5f27fe5d9b200a5a3d5d728a7ba54096839faa3
kineticstorytelling <kineticstorytelling@gmail.com>
2024-04-19 23:52:30 -0400

    moved posts into index article

--- index.php
+++ index.php
@@ -5,19 +5,22 @@
 
 		<article class="content px-3 py-5 p-md-5">
         
+            <?php
+                if( have_posts ()){
+                     
+                    while( have_posts() ){
+                        
+                        the_post();
+                        
+                        get_template_part('template-parts/content', 'archive');
+                    }
+                }
+            ?>
+
+            <?php
+
+            ?>
 	    </article>
 
-        <?php
-            if( have_posts ()){
-                 
-                while( have_posts() ){
-                    
-                    the_post();
-                    
-                    get_template_part('template-parts/content', 'archive');
-                }
-            }
-        ?>
-    <?php
     get_footer();
     ?>
--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -6,6 +6,7 @@
 	<div class="media-body">
 		<h3 class="title mb-1"><?php the_title(); ?></h3>
 		<div class="meta mb-1"><span class="date"><?php the_date(); ?></span><span class="time">5 min read</span><span class="comment"><a href="#"><?php comments_number();?></a></span></div>
+		
 		<div class="intro">
             <?php
             the_excerpt();
