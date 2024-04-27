397cba476f38e50502a9b558009a8c38a8040766
kineticstorytelling <kineticstorytelling@gmail.com>
2024-04-26 21:54:19 -0400

    title change

--- functions.php
+++ functions.php
@@ -28,7 +28,7 @@ add_action('init', 'luke_menus');
 function luke_register_styles(){
     $version = wp_get_theme()->get('Version');
     wp_enqueue_style('luke-style', get_template_directory_uri() . "/style.css", array('followandrew-bootstrap'), $version, 'all');
-    wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+    wp_enqueue_style('luke-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
     wp_enqueue_style('followandrew-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 
 }
